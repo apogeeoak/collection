5226b520cc99fc440549b3ab0267e8b7130fc77c
apogeeoak <59737221+apogeeoak@users.noreply.github.com>
2026-04-16 21:03:29 -0400

    build(deps): update rand

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,12 @@ pub fn main() -> Result<(), Box<dyn Error>> {
     shuffle_items(&mut config);
 
     // Format output.
-    let output = format!("{}\n\n{}\n{}", config.title, format_labels(&config), format_items(&config));
+    let output = format!(
+        "{}\n\n{}\n{}",
+        config.title,
+        format_labels(&config),
+        format_items(&config)
+    );
 
     println!("\n{}", output);
     write_to_file(config.output_file, output);
@@ -27,23 +32,35 @@ pub fn main() -> Result<(), Box<dyn Error>> {
 
 fn shuffle_items(config: &mut Config) {
     // Shuffle items in place.
-    let mut rng = rand::thread_rng();
+    let mut rng = rand::rng();
     for collection in config.collections.iter_mut() {
         collection.items.shuffle(&mut rng);
     }
 }
 
 fn format_labels(config: &Config) -> String {
-    let labels = config.collections.iter().map(|c| Row { output: &c.label, width: c.width.into() });
+    let labels = config.collections.iter().map(|c| Row {
+        output: &c.label,
+        width: c.width.into(),
+    });
     let widths = config.collections.iter().map(|c| c.width.into());
 
-    format!("{}\n{}", library::format::row(labels), library::format::divider(widths, None))
+    format!(
+        "{}\n{}",
+        library::format::row(labels),
+        library::format::divider(widths, None)
+    )
 }
 
 fn format_items(config: &Config) -> String {
     let mut builder = String::new();
     let string_empty = String::new();
-    let length = config.collections.iter().map(|c| c.items.len()).max().unwrap_or(0);
+    let length = config
+        .collections
+        .iter()
+        .map(|c| c.items.len())
+        .max()
+        .unwrap_or(0);
     for index in 0..length {
         let items = config.collections.iter().map(|c| Row {
             output: c.items.get(index).unwrap_or(&string_empty),
@@ -58,7 +75,9 @@ fn format_items(config: &Config) -> String {
 fn wait_for_user() {
     print!("Press <enter> to exit: ");
     stdout().flush().expect("Error writing output.");
-    stdin().read_line(&mut String::new()).expect("Error reading input.");
+    stdin()
+        .read_line(&mut String::new())
+        .expect("Error reading input.");
 }
 
 fn write_to_file<P, C>(path: P, contents: C)
